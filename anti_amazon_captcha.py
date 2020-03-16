c397b19a8be64b12366eddfeb7792c08a8095310
william <william.kuo@viscovery.com>
2020-03-16 10:17:57 +0800

    fix crop infinity loop bug

--- anti_amazon_captcha.py
+++ anti_amazon_captcha.py
@@ -22,15 +22,17 @@ def anti_amazon_captcha(image_path):
         col_mean = total / height
         col_mean_list.append(col_mean)
 
+    retry_count = 0
     threshold = 241
     separator_index_list = list()
     while len(separator_index_list) != 7:
-        if len(separator_index_list) > 7:
+        retry_count += 1
+        if threshold >= 255 or threshold <= 0 or retry_count >= 15:
+            return 'can not crop correct number of letters'
+        elif len(separator_index_list) > 7:
             threshold += 1
         elif len(separator_index_list) < 7:
             threshold -= 1
-        elif threshold >= 255 or threshold <= 0:
-            return 'can not crop correct number of letters'
 
         col_mean_index_list = list()
         for i, col_mean in enumerate(col_mean_list):
@@ -88,15 +90,16 @@ def anti_amazon_captcha(image_path):
     return result
 
 
-# NOTE: some captcha still fail due to crop not correct
+# NOTE: test
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha0.jpg'), 'BTRPRK'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha1.jpg'), 'FYKLXE'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha2.jpg'), 'MXRCTR'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha3.jpg'), 'MBEPFA'))
-print('{} == {}'.format(anti_amazon_captcha('amazon_captcha4.jpg'), 'GRJBUY'))
+print('{} == {}'.format(anti_amazon_captcha('amazon_captcha4.jpg'), 'GRJBUY'))  # fail  # noqa
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha5.jpg'), 'LACNMU'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha6.jpg'), 'LXUJEP'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha7.jpg'), 'JEPMRY'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha8.jpg'), 'BTCAJK'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha9.jpg'), 'BXYHJX'))
 print('{} == {}'.format(anti_amazon_captcha('amazon_captcha10.jpg'), 'HXEJFM'))
+print('{} == {}'.format(anti_amazon_captcha('amazon_captcha11.jpg'), 'UUGEYE'))  # crop infinity loop  # noqa
